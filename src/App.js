060f8965243c6d04c199028f431ad559ea18c480
prgmr99 <johnyeom24@naver.com>
2023-11-13 08:54:30 +0900

    feat: 식당 예상 방문 날짜 입력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView";
+
 class App {
-  async run() {}
+  async run() {
+    await this.intro();
+    await this.getDate();
+  }
+
+  async intro() {
+    Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+  }
+
+  async getDate() {
+    InputView.readDate().then((result) => {
+      console.log(result);
+    });
+  }
 }
 
 export default App;
