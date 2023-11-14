be329e6efd3adb20d1758a12725ad45602bc519b
prgmr99 <johnyeom24@naver.com>
2023-11-14 20:09:44 +0900

    feat: 공백을 입력했을 경우, 공백이 포함된 경우

--- src/validation.js
+++ src/validation.js
@@ -1,10 +1,16 @@
 const isValidDate = (date) => {
   const dateNumber = Number(date);
   if (!Number.isFinite(dateNumber)) {
-    throw new Error("[ERROR] 숫자만 입력해주세요.");
+    throw new Error("[ERROR] 숫자가 아닙니다. 다시 입력해 주세요.");
   }
   if (dateNumber < 1 && dateNumber > 31) {
     throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+  }
+  if (date !== date.trim()) {
+    throw new Error("[ERROR] 공백이 포함되어 있습니다. 다시 입력해 주세요.");
+  }
+  if (date.trim() === "") {
+    throw new Error("[ERROR] 공백만 입력할 수 없습니다. 다시 입력해 주세요.");
   }
 };
 
